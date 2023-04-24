08e7dfaeb2d0e6224c560b79797f9101ec2cd077
LiyanaRoslie <liyana.roslie@gmail.com>
2023-04-25 00:12:43 +0800

    remove children=

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import dash_html_components as html
 
 app=dash.Dash()
 
-app.layout = html.Div(children=[
-    html.H1(children="World Happiness Dashboard"),
+app.layout = html.Div([
+    # can remove children=
+    #html.H1(children="World Happiness Dashboard"),
+    html.H1("World Happiness Dashboard"),
     html.P(['This dashboard shows the happiness score',
             html.Br(),
             html.A('World Happiness Report Data Source',
