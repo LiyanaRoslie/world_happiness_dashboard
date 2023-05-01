aed79b5d98de8f08a5735529baef4973d73cdf52
LiyanaRoslie <liyana.roslie@gmail.com>
2023-05-01 19:03:33 +0800

    show graph base on country dropdown list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash
 from dash import html
 from dash import dcc
+from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
 
@@ -24,12 +25,25 @@ app.layout = html.Div([
             html.A('World Happiness Report Data Source',
                    href='https://worldhappiness.report/',
                    target='_blank')]),
-    dcc.RadioItems(options=region_options, value='North America'),
-    dcc.Checklist(options=region_options, value=['North America']),
-    dcc.Dropdown(options=country_options, value='United States'),
+    #dcc.RadioItems(options=region_options, value='North America'),
+    #dcc.Checklist(options=region_options, value=['North America']),
+    dcc.Dropdown(id='country-dropdown', options=country_options, value='United States'),
     #manipulate using pandas dataframe
-    dcc.Graph(figure=line_fig)
+    dcc.Graph(id='happiness-graph')
 ])
+
+@app.callback(
+    #shortform
+    #Output('happiness-graph', 'figure'),
+    #Input('country-dropdown', 'value')
+    Output(component_id='happiness-graph', component_property='figure'),
+    Input(component_id='country-dropdown', component_property='value')
+)
+def update_graph(selected_country):
+    filtered_happiness = happiness[happiness['country']==selected_country]
+    line_fig=px.line(filtered_happiness, x='year', y='happiness_score',
+                     title=f'Happiness Score in {selected_country}')
+    return line_fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
